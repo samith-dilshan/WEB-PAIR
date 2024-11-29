f6e2d0f499fc6196e75c4c84957fce5280864f75
samith-dilshan <my.koyeb.ac@gmail.com>
2024-11-29 17:22:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const bodyParser = require("body-parser");
 const app = express();
-const path = require('path');
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/code', code);
 
-// Set static and JSON parsers
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`⏩ Server running on http://localhost:` + PORT)
+})
 
-// Serve pair.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pair.html'));
-});
-
-// Define other routes
-app.use('/code', require('./pair'));
-
-// Export as a Vercel-compatible handler
-module.exports = app;
+module.exports = app
